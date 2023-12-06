3c5b2b4436c72512a61e50d4a460a1a38cca8b81
unknown <phamha2003j@gmail.com>
2023-12-06 17:02:10 +0700

    CK-CNJV

--- src/main/java/com/example/demo/controller/salespeopleChoose.java
+++ src/main/java/com/example/demo/controller/salespeopleChoose.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.model.CartData;
+import com.example.demo.model.Customer;
 import com.example.demo.model.Product;
 import com.example.demo.model.Purchase;
 import com.example.demo.model.User;
@@ -73,30 +76,79 @@ public class salespeopleChoose {
 	
 	}
 	@GetMapping("/checkout")
-	public String checkout(HttpSession session,Principal princ) {
-		
-		  User u = userService.findbyUsername(princ.getName()); // Replace with the desired userId
-		  	String userId = u.getId() + "";
-		    // Retrieve the map from the session using the userId as the key
-		    Map<String, List<CartData>> userMap = (Map<String, List<CartData>>) session.getAttribute("cart");
-		    // Check if the userMap exists
-		    if (userMap != null) {
-		        List<CartData> cartDataList = userMap.get(userId);
-		        for (CartData cartData : cartDataList) {
-		            System.out.println("Product Name: " + cartData.getName());
-		            // Print other properties of CartData as needed
-		            // ...
-		        }
-		    }
-		    else
-		    {
-		    	System.out.println("ko có gì");
-		    }
+	public String checkout(HttpSession session, Principal princ, Model model) {
+	   int total = 0;
+	   int quantity=0;
+		User user = userService.findbyUsername(princ.getName());
+	    String userId = user.getId() + "";
+
+	    Object attribute = session.getAttribute("cart");
+
+	    if (attribute instanceof List) {
+	        List<Map<String, List<CartData>>> values = (List<Map<String, List<CartData>>>) attribute;
+
+	        for (Map<String, List<CartData>> map : values) {
+	            if (map.containsKey(userId)) {
+	                List<CartData> cartDataList = map.get(userId);
+
+	                for ( CartData map2 : cartDataList) {
+	                	quantity += map2.getCount();
+						total += map2.getTotal();
+					}
+	                
+	                session.removeAttribute("cart");
+	                break; 
+	            }
+	        }
+	    } else if (attribute instanceof Map) {
+	        Map<String, List<CartData>> map = (Map<String, List<CartData>>) attribute;
+
+	        if (map.containsKey(userId)) {
+	            List<CartData> cartDataList = map.get(userId);
+	            for ( CartData map2 : cartDataList) {
+                	quantity += map2.getCount();
+					total += map2.getTotal();
+				}
+	        }
+	    } else {
+	        System.out.println("Không có gì trong session");
+	    }
+	    	
+	    model.addAttribute("Total", total);
+	    model.addAttribute("Quantity", quantity);
+	    
 
 	    return "checkout";
 	}
 	
+	@PostMapping("/check-phone")
+	public ResponseEntity<?> checkout(@RequestParam("phoneNumber") String phoneNumber) {
+	    Customer customer = customerRepository.findByPhoneNumber(phoneNumber);
+
+	    if (customer != null) {
+	        Map<String, Object> response = new HashMap<>();
+	        response.put("found", true);
+	        response.put("fullName", customer.getName());
+	        response.put("address", customer.getAddress());
+	        return ResponseEntity.ok(response);
+	    } else {
+	        Map<String, Object> response = new HashMap<>();
+	        response.put("found", false);
+	        return ResponseEntity.ok(response);
+	    }
+	}
 	
+	@PostMapping("/get-customer-infor")
+		
+	public String getCusomer(@RequestParam("name")String name,@RequestParam("address")String address ,@RequestParam("phoneNumber")String phone)
+	{
+		Customer c = new Customer();
+		c.setAddress(address);
+		c.setName(name);
+		c.setPhoneNumber(phone);
+		customerRepository.save(c);
+		return "redirect:/checkout";
+	}
 	
 
 }
---
+++ src/main/resources/static/JS/checkout.js
@@ -0,0 +1,34 @@
+var form = document.querySelector('form');
+var resultDiv = document.getElementById('result');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var url = '/check-phone';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'phoneNumber=' + encodeURIComponent(phoneNumber)
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (result.found) {
+                // Display customer information if customer is found
+                resultDiv.innerHTML = `
+                    <p>Tên khách hàng: ${result.fullName}</p>
+                    <p>Địa chỉ: ${result.address}</p>
+                `;
+                console.log("succes");
+            } else {
+                // Display a form if customer is not found
+                resultDiv.innerHTML = '<form action="/get-customer-infor" method="POST"> <label for="name">Name:</label>   <input type="text" id="name" name="name" required> <label for="phoneNumber">Phone Number:</label>    <input type="text" id="phoneNumber" name="phoneNumber" required>  <label for="address">Address:</label> <input type="text" id="address" name="address" required>   <button type="submit">Checkout</button></form>';
+                                console.log("succes")
+
+            }
+        })
+        .catch(error => console.log(error));
+});
